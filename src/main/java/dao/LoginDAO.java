0526389e81ed4fb342cf5c54932a96b76d478da6
motok <154608951+saiki-m@users.noreply.github.com>
2024-06-23 10:20:04 +0900

    空白を入れて、見やすくしました。

--- src/main/java/dao/LoginDAO.java
+++ src/main/java/dao/LoginDAO.java
@@ -14,53 +14,46 @@ import beans.AccountBean;
 //JDBC_URL、DB_USER、DB_PASSがLoginDAOクラスで使えるようになる。
 public class LoginDAO extends ConfigDB{
   
-  //６５行目の「return accountID」ができるよう初期設定しておく。
-  AccountBean account = null;
-  
-  
-  public AccountBean findAccount(String name, String password) {
-	 
-	//親クラスConfigDBのメソッドを利用
-	ReadJDBC_Driver();
+    public AccountBean findAccount(String name, String password) {
+
+	    ReadJDBC_Driver();
     
-    // データベースへ接続
-    try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
+	    // データベースへ接続
+	    try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
     	
-      // SELECT文を準備。
-      // アカウントテーブルからアカウント名が一致し、パスワードも一致するアカウントIDを表示。
-      String sql = "SELECT アカウントID, アカウント名, パスワード, メールアドレス, 秘密の質問 FROM アカウント WHERE アカウント名=? AND パスワード=?";
-        
-      PreparedStatement pStmt = conn.prepareStatement(sql);
-      
-      //WHERE文の?に代入
-      pStmt.setString(1, name);
-      pStmt.setString(2, password);
+	    	AccountBean account = null;
+	    	
+	        // アカウントテーブルからアカウント名が一致し、パスワードも一致するアカウントIDを表示。
+	        String sql = "SELECT アカウントID, アカウント名, パスワード, メールアドレス, 秘密の質問 FROM アカウント WHERE アカウント名=? AND パスワード=?";
+	        
+	        PreparedStatement pStmt = conn.prepareStatement(sql);
+	      
+	        //WHERE文の?に代入
+	        pStmt.setString(1, name);
+	        pStmt.setString(2, password);
 
       
-      // SELECTを実行し、結果表を取得
-      ResultSet rs = pStmt.executeQuery();
+		    // SELECTを実行し、結果表を取得
+		    ResultSet rs = pStmt.executeQuery();
       
-	      // 結果表にあるアカウントIDをaccountIDインスタンスに保存。
-	      while (rs.next()) {
-	        int ID = rs.getInt("アカウントID");
-	        String accountName = rs.getString("アカウント名");
-	        String pass = rs.getString("パスワード");
-	        String mailAd = rs.getString("メールアドレス");
-	        String secret_q = rs.getString("秘密の質問");
-	        
-	        account = new AccountBean(ID, accountName, pass, mailAd, secret_q);
-	      }
-	      
-	      
-    }  
-      //tryの中でエラーが出たら、catchのみ実行する
-    catch (SQLException e) {
-      e.printStackTrace();
-    return null;   //Login.javaの35行目、dao.findAccountID(account)にnullが入る。
-    }                
-    
-  //アカウントIDを取得できたとき
-  return account;   //accountIDインスタンスにアカウントIDが入っている状態。
-                          
-  }
+	        // 結果表にあるアカウントIDをaccountIDインスタンスに保存。
+	        if(rs.next()) {
+		        int ID = rs.getInt("アカウントID");
+		        String accountName = rs.getString("アカウント名");
+		        String pass = rs.getString("パスワード");
+		        String mailAd = rs.getString("メールアドレス");
+		        String secret_q = rs.getString("秘密の質問");
+		        
+		        account = new AccountBean(ID, accountName, pass, mailAd, secret_q);
+	        }
+
+	    return account;   //accountIDインスタンスにアカウントIDが入っている状態。
+	    
+	    }  
+	    catch (SQLException e) {
+	      e.printStackTrace();
+	    
+	    return null;   //Login.javaの35行目、dao.findAccountID(account)にnullが入る。
+	    }
+    }
 }
